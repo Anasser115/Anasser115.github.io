a36e9f449cdd6260779e4b91231c87f4d5c0ba72
Abdelrahman Hussein <ahussein@dealerpolicy.com>
2022-02-22 15:15:01 -0500

    Added About page

---
+++ src/components/aboutMePage.js
@@ -0,0 +1,11 @@
+export default function AboutMe() {
+  return (
+    <main className="aboutMe">
+      <section >
+        <h1>About Me</h1>
+        <p>Labore adipisicing nisi qui sint. Et aliqua amet elit aliqua in aute cupidatat. Laboris in Lorem et labore cillum esse.</p>
+      </section>
+
+    </main>
+  );
+}
