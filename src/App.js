85774c6884b9e4f51c3eb847748cd524d1555de9
Abdelrahman Hussein <ahussein@dealerpolicy.com>
2022-02-22 15:14:43 -0500

    Removed routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import './style/App.css';
 import './style/fonts/PlusJakartaSans-Bold.ttf';
 import './style/fonts/PlusJakartaSans-Medium.ttf';
 import LandingPage from './components/landingPage/landingPage.js';
-import ToolBar from './components/landingPage/toolBar.js';
+import AboutMe from './components/aboutMePage.js';
 import { Routes , Link, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="Apps">
-    <Routes>
-       <Route path='' element={<LandingPage/>} />
-
-    </Routes>
+      <LandingPage/>
+      <AboutMe/>
     </div>
   );
 }
