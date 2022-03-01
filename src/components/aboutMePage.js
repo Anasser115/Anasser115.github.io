8aef611c284152af47e396b0560443d85e324d5d
Abdelrahman Hussein <ahussein@dealerpolicy.com>
2022-03-01 14:03:59 -0500

    About me section added

--- src/components/aboutMePage.js
+++ src/components/aboutMePage.js
@@ -2,11 +2,31 @@ export default function AboutMe() {
   return (
     <main className="aboutMe">
       <section className="aboutMeContainer">
-        <h1>About Me</h1>
-        <p>Labore adipisicing nisi qui sint. Et aliqua amet elit aliqua in aute cupidatat. Laboris in Lorem et labore cillum esse.</p>
+        <h1 className="aboutMeTitle">About Me</h1>
+        <article  className="aboutMeText">
+        <p>
+          An enthusiastic software engineer currently shaping the future of
+          software development by designing smooth user-interfaces that promote
+          user interaction with information and data. While traveling around the
+          world.
+        </p>
+        <p>
+          I've always been passionate about colors, design learning about new
+          technologies. Other than sitting in from of my display I enjoy myself
+          in hiking and taking photo a long the way. I'm grateful that my job
+          allows me to work from anywhere.
+        </p>
+        <p>
+          I specialize in building web applications, leading front-end teams and
+          developing visual design systems. I enjoy creating effortless user
+          experience and designing delightful digital products. The entire
+          process of going from a concept to release and gathering user's
+          feedback on either client's or my own products is what makes me wake
+          up everyday!
+        </p>
+        </article>
+      <div className="BlackBox">elnhn</div>
       </section>
-        <div className="BlackBox">elnhn</div>
-
     </main>
   );
 }
