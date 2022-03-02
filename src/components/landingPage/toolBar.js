8d35dd16e6b2b1c87fe9619a3e8e5eb17c21166a
Abdelrahman Hussein <ahussein@dealerpolicy.com>
2022-03-02 02:51:07 -0500

    added fukction to buttons

--- src/components/landingPage/toolBar.js
+++ src/components/landingPage/toolBar.js
@@ -1,13 +1,20 @@
+import { Link } from 'react-scroll';
+
+
+
 export default function ToolBar() {
+
+
   return (
-    <main className="toolBar">
-            <h2> عبد الرحمن</h2>
+
+    <main className="toolBarContainer">
         <h1 className="myName">ABDELRAHMAN HUSSEIN<a className="flickeringDot">.</a></h1>
-      <section className="toolBarButtons">
-         <button>About</button>
-         <button>Work</button>
-         <button>Contact</button>
+      <section className="toolBar">
+         <button  className="toolBarButtons"><Link className="buttonPadding" to='aboutPage' smooth={true} duration={1500}>About</Link></button>
+         <button  className="toolBarButtons"><Link className="buttonPadding"  to='myWorkPage' smooth={true} duration={1500}>Work</Link></button>
+         <button  className="toolBarButtons"><Link className="buttonPadding"  to='contactMePage' smooth={true} duration={1500}>Contact</Link></button>
       </section>
     </main>
+
   );
 }
